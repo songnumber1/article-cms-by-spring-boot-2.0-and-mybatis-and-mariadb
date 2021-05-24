c973359184bd3abbd4063ed255c72ccf635bc258
songnumber1 <songnumber1@naver.com>
2021-05-24 22:36:13 +0900

    게시물리스트를  자바버전과 el, jstl 버전 2가지로생성

--- src/main/java/com/sbs/starter/controller/ArticleController.java
+++ src/main/java/com/sbs/starter/controller/ArticleController.java
@@ -2,8 +2,11 @@ package com.sbs.starter.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sbs.starter.dto.Article;
@@ -17,11 +20,24 @@ public class ArticleController {
 	@Autowired
 	ArticleService articleService;
 	
+//	@RequestMapping("/article/list")
+//	public String showMain(HttpServletRequest request) {
+//		List<Article> list = articleService.getList();
+//		
+//		request.setAttribute("list", list);
+//		
+//		System.out.println("1");
+//		
+//		return "article/list";
+//	}
+	
 	@RequestMapping("/article/list")
-	public String showMain() {
+	public String showMain(Model aModel) {
 		List<Article> list = articleService.getList();
 		
-		log.info("list : " + list);
+		aModel.addAttribute("list", list);
+		// 위의 showMain() 함수와 같이 
+		// request.setAttribute("list", list);와 똑같은 표현이다.
 		
 		return "article/list";
 	}
--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -11,8 +11,8 @@ import com.sbs.starter.dto.Article;
 public class ArticleServiceImpl implements ArticleService {
 	public List<Article> getList() {
 		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1", "내용2");
-		Article article2 = new Article(1, "2019-08-20 12:12:13", "제목2", "내용2");
-		Article article3 = new Article(1, "2019-08-20 12:12:14", "제목3", "내용3");
+		Article article2 = new Article(2, "2019-08-20 12:12:13", "제목2", "내용2");
+		Article article3 = new Article(3, "2019-08-20 12:12:14", "제목3", "내용3");
 		
 		List<Article> list = new ArrayList<>();
 		
