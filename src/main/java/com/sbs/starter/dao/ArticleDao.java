2695992129e47437b2d40ba23193f163ec4e5d39
songnumber1 <songnumber1@naver.com>
2021-05-25 22:06:11 +0900

    ArticelDaoImpl을 제거하고 마이바티스가 대신 구현하도록 처리(마이바티스는 데이터를 DB에서 가져옴, 즉 이제 가짜 데이는 끝)

--- src/main/java/com/sbs/starter/dao/ArticleDao.java
+++ src/main/java/com/sbs/starter/dao/ArticleDao.java
@@ -2,8 +2,11 @@ package com.sbs.starter.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.sbs.starter.dto.Article;
 
+@Mapper
 public interface ArticleDao {
 	public List<Article> getList();	
 }
