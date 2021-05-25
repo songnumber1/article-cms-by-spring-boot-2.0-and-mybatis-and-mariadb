bda8eb9574248c1ced7e14533f3390f9f975d9e0
songnumber1 <songnumber1@naver.com>
2021-05-25 21:10:10 +0900

    ArticleDao 생성 및 적용

---
+++ src/main/java/com/sbs/starter/dao/ArticleDaoImpl.java
@@ -0,0 +1,29 @@
+package com.sbs.starter.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.sbs.starter.dto.Article;
+
+@Component
+public class ArticleDaoImpl implements ArticleDao {
+
+	@Override
+	public List<Article> getList() {
+		// TODO Auto-generated method stub
+		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1", "내용2");
+		Article article2 = new Article(2, "2019-08-20 12:12:13", "제목2", "내용2");
+		Article article3 = new Article(3, "2019-08-20 12:12:14", "제목3", "내용3");
+		
+		List<Article> list = new ArrayList<>();
+		
+		list.add(article3);
+		list.add(article2);
+		list.add(article1);
+		
+		return list;
+	}
+
+}
--- src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
+++ src/main/java/com/sbs/starter/service/ArticleServiceImpl.java
@@ -3,23 +3,18 @@ package com.sbs.starter.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sbs.starter.dao.ArticleDao;
 import com.sbs.starter.dto.Article;
 
 @Service
 public class ArticleServiceImpl implements ArticleService {
+	@Autowired
+	ArticleDao articleDao;
+	
 	public List<Article> getList() {
-		Article article1 = new Article(1, "2019-08-20 12:12:12", "제목1", "내용2");
-		Article article2 = new Article(2, "2019-08-20 12:12:13", "제목2", "내용2");
-		Article article3 = new Article(3, "2019-08-20 12:12:14", "제목3", "내용3");
-		
-		List<Article> list = new ArrayList<>();
-		
-		list.add(article3);
-		list.add(article2);
-		list.add(article1);
-		
-		return list;
+		return articleDao.getList();
 	}
 }
